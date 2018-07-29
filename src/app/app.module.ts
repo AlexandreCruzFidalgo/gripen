60946b72d2d476493a34dc2302298663a2868c27
Alexandre Fidalgo <alexandre.fidalgo@quality.com.br>
2018-07-29 11:26:38 -0300

    estudo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BasicComponent } from './basic/basic.component';
+import { AdminModule } from './admin/admin.module';
+import { AlertModule } from 'ngx-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [AlertModule.forRoot(),
+    BrowserModule, AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
